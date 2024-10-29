97622038f6f9c3f7125ee8a68a765426bbca1052
nisim <nisimohana44@gmail.com>
2024-10-29 16:36:16 +0200

    hel

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 // app.js
 
-require("dotenv").config({ path: "./config.env" });
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const app = express();
-app.use(cors());
+require("dotenv").config({ path: "./config.env" })
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+const app = express()
+app.use(cors())
 
 // יבוא של ה-Routes
-const pdfRoutes = require("./routes/pdfRoutes");
-const askRoutes = require("./routes/askRoutes");
+const pdfRoutes = require("./routes/pdfRoutes")
+const askRoutes = require("./routes/askRoutes")
 
 //middleWares
-app.use(express.json());
-app.use("/pdf", pdfRoutes);
-app.use("/ask", askRoutes);
-app.use("/pdf", express.static(path.join(__dirname, "data")));
+app.use(express.json())
+app.use("/pdf", pdfRoutes)
+app.use("/ask", askRoutes)
+app.use("/pdf", express.static(path.join(__dirname, "data")))
 
 //Server running
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+}) // log port
